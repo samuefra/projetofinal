6049c37e22a0a3ce711c719a5d32ee2959cfbbfd
Samuel Efraim <samuefra@gmail.com>
2022-02-25 18:19:04 -0300

    mudanças finais

--- projeto.R
+++ projeto.R
@@ -26,8 +26,6 @@ install.packages("lexiconPT")
 library(lexiconPT)
 library(naivebayes)
 library(lattice)
-library(Rstem)
-library(sentiment)
 library(httr)
 library(twitteR)
 library(ROAuth)
@@ -59,11 +57,37 @@ access_token <- "1496765351215964161-OUdq8uwksz01Bm1rKPfT0ug0NuGlhX"
 access_token_secret <- "l7dBK3KMtOFrDyCzL7welnS8yZgpBmR0qSb8raukA3J3m"
 setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 
-#obtendo tweets em portugues
-tweets <- searchTwitter("Bolsonaro russia", n=20000, lang="pt")
+#obtendo tweets em portugues contendo bolsonaro e russia
+tweets <- searchTwitter("Bolsonaro russia", n=10000, lang="pt")
 n.tweets <- length(tweets)
 tweets.df <- twListToDF(tweets)
 tweets.txt <- sapply(tweets, function(t)t$getText())
+
+##### tweets contendo Bolsonaro pré- invasão
+tweetsantes <- searchTwitter("Bolsonaro", n=5000, lang="pt",since ="2022-02-10" ,until = "2022-02-22"  )
+n.tweetsa <- length(tweetsantes)
+tweetsantes.df <- twListToDF(tweetsantes)
+tweetsantes.txt <- sapply(tweetsantes, function(t)t$getText())
+#### checando horario se +10000 tweets depois da invasão não preciso usar since e until
+tweets.df %<>% 
+  mutate(
+    created = created %>% 
+      str_remove_all(pattern = '\\+0000') %>%
+      parse_date_time(orders = '%y-%m-%d %H%M%S')
+  )
+
+tweets.df %<>% 
+  mutate(Created_At_Round = created%>% round(units = 'hours') %>% as.POSIXct())
+
+tweets.df %>% pull(created) %>% min()
+
+tweets.df %>% pull(created) %>% max()
+##pós invasão 
+tweetsdepois <- searchTwitter("Bolsonaro", n=5000, lang="pt" )
+n.tweetsdepois <- length(tweetsdepois)
+tweetsdepois.df <- twListToDF(tweetsdepois)
+tweetsdepois.txt <- sapply(tweetsdepois, function(t)t$getText())
+
 
 # função limpando os tweets
 clean.text = function(x)
@@ -97,7 +121,7 @@ clean.text = function(x)
   return(x)
 }
 
-#aplicando clean text 
+#aplicando clean text com russia
 cleanText <- clean.text(tweets.txt)
 idx <- which(cleanText == " ")
 cleanText <- cleanText[cleanText != " "]
@@ -109,23 +133,78 @@ tweets.df %<>%
       # Parse date.
       parse_date_time(orders = '%y-%m-%d %H%M%S')
   )
+#aplicando clean text antes da invasão
+cleanTexta <- clean.text(tweetsantes.txt)
+idy <- which(cleanTexta == " ")
+cleanTexta <- cleanTexta[cleanTexta != " "]
+tweetsantes.df %<>% 
+  mutate(
+    created = created %>% 
+      # Remove zeros.
+      str_remove_all(pattern = '\\+0000') %>%
+      # Parse date.
+      parse_date_time(orders = '%y-%m-%d %H%M%S')
+  )
+
+#aplicando clean text depois da invasão
+cleanTextd <- clean.text(tweetsdepois.txt)
+idz <- which(cleanTextd == " ")
+cleanTextd <- cleanTextd[cleanTexta != " "]
+tweetsdepois.df %<>% 
+  mutate(
+    created = created %>% 
+      # Remove zeros.
+      str_remove_all(pattern = '\\+0000') %>%
+      # Parse date.
+      parse_date_time(orders = '%y-%m-%d %H%M%S')
+  )
 
 
 
 
-##cloud
+##cloud com russia
 text_corpus <- Corpus(VectorSource(cleanText))
 text_corpus <- tm_map(text_corpus, content_transformer(tolower))
 text_corpus <- tm_map(text_corpus, function(x)removeWords(x,stopwords("portuguese")))
-text_corpus <- tm_map(text_corpus, removeWords, c("rússia","bolsonaro"))
+text_corpus <- tm_map(text_corpus, removeWords, c("messias" , "jair", "rússia","bolsonaro", "brasileiros", "brasil", "que", "foi", "para", "rú", "rúss" ))
 tdm <- TermDocumentMatrix(text_corpus)
 tdm <- as.matrix(tdm)
 tdm <- sort(rowSums(tdm), decreasing = TRUE)
 tdm <- data.frame(word = names(tdm), freq = tdm)
+set.seed(113)
+dev.off()
+wordcloud(text_corpus, min.freq = 10, max.words = 200, scale = c(2,0.5,0.25),
+          colors=brewer.pal(7, "Dark2"), random.color = T, random.order = F)
+#cloud antes
+text_corpusa <- Corpus(VectorSource(cleanTexta))
+text_corpusa <- tm_map(text_corpusa, content_transformer(tolower))
+text_corpusa <- tm_map(text_corpusa, function(x)removeWords(x,stopwords("portuguese")))
+text_corpusa <- tm_map(text_corpusa, removeWords, c("messias" , "jair","rússia","bolsonaro", "brasileiros", "brasil", "que", "foi", "para", "rú", "rúss"))
+tdma <- TermDocumentMatrix(text_corpusa)
+tdma <- as.matrix(tdma)
+tdma <- sort(rowSums(tdma), decreasing = TRUE)
+tdma <- data.frame(word = names(tdma), freq = tdma)
+dev.off()
 set.seed(123)
-wordcloud(text_corpus, min.freq = 1, max.words = 100, scale = c(3.1,1),
-          colors=brewer.pal(8, "Dark2"), random.color = T, random.order = F)
-###frquency
+wordcloud(text_corpusa, min.freq = 10, max.words = 200, scale = c(2,0.5,0.25),
+          colors=brewer.pal(7, "Dark2"), random.color = T, random.order = F)
+#cloud depois
+
+text_corpusd <- Corpus(VectorSource(cleanTextd))
+text_corpusd <- tm_map(text_corpusd, content_transformer(tolower))
+text_corpusd <- tm_map(text_corpusd, function(x)removeWords(x,stopwords("portuguese")))
+text_corpusd <- tm_map(text_corpusd, removeWords, c("messias" , "jair","rússia","bolsonaro", "brasileiros", "brasil", "que", "foi", "para", "rú", "rúss"))
+tdmd <- TermDocumentMatrix(text_corpusd)
+tdmd <- as.matrix(tdmd)
+tdmd <- sort(rowSums(tdmd), decreasing = TRUE)
+tdmd <- data.frame(word = names(tdmd), freq = tdmd)
+dev.off()
+set.seed(123)
+wordcloud(text_corpusd, min.freq = 10, max.words = 200, scale = c(2,0.5,0.25),
+          colors=brewer.pal(7, "Dark2"), random.color = T, random.order = F)
+
+
+###frquency com russia
 ggplot(tdm[1:20,], aes(x=reorder(word, freq), y=freq)) + 
   geom_bar(stat="identity") +
   xlab("Termos") + 
@@ -134,115 +213,30 @@ ggplot(tdm[1:20,], aes(x=reorder(word, freq), y=freq)) +
   theme(axis.text=element_text(size=7)) +
   ggtitle('Termos mais frequentes') +
   ggeasy::easy_center_title()
-## bigram
 
-bi.gram.words <- tweets.df %>% 
-  unnest_tokens(
-    input = text, 
-    output = bigram, 
-    token = 'ngrams', 
-    n = 2
-  ) %>% 
-  filter(! is.na(bigram))
+###frquency antes
 
-bi.gram.words %>% 
-  select(bigram) %>% 
-  head(10)
-extra.stop.words <- c('https')
-stopwords.df <- tibble(
-  word = c(stopwords(kind = 'es'),
-           stopwords(kind = 'pt'),
-           extra.stop.words)
-)
+ggplot(tdma[1:20,], aes(x=reorder(word, freq), y=freq)) + 
+  geom_bar(stat="identity") +
+  xlab("Termos") + 
+  ylab("frequência") + 
+  coord_flip() +
+  theme(axis.text=element_text(size=7)) +
+  ggtitle('Termos mais frequentes antes da invasão') +
+  ggeasy::easy_center_title()
 
-bi.gram.words %<>% 
-  separate(col = bigram, into = c('word1', 'word2'), sep = ' ') %>% 
-  filter(! word1 %in% stopwords.df$word) %>% 
-  filter(! word2 %in% stopwords.df$word) %>% 
-  filter(! is.na(word1)) %>% 
-  filter(! is.na(word2)) 
+###frquency depois
 
-bi.gram.count <- bi.gram.words %>% 
-  dplyr::count(word1, word2, sort = TRUE) %>% 
-  dplyr::rename(weight = n)
+ggplot(tdmd[1:20,], aes(x=reorder(word, freq), y=freq)) + 
+  geom_bar(stat="identity") +
+  xlab("Termos") + 
+  ylab("frequência") + 
+  coord_flip() +
+  theme(axis.text=element_text(size=7)) +
+  ggtitle('Termos mais frequentes depois da invasão') +
+  ggeasy::easy_center_title()
 
-bi.gram.count <- bi.gram.count%>%head(10)
-save(bi.gram.count,file = "bigram.rds")
-
-
-
-threshold <- 1200
-
-# For visualization purposes we scale by a global factor. 
-ScaleWeight <- function(x, lambda) {
-  x / lambda
-}
-
-network <-  bi.gram.count %>%
-  filter(weight > threshold) %>%
-  mutate(weight = ScaleWeight(x = weight, lambda = 2E3)) %>% 
-  graph_from_data_frame(directed = FALSE)
-
-
-
-
-V(network)$degree <- strength(graph = network)
-
-# Compute the weight shares.
-E(network)$width <- E(network)$weight/max(E(network)$weight)
-
-plot(
-  network, 
-  vertex.color = 'lightblue',
-  # Scale node size by degree.
-  vertex.size = 2*V(network)$degree,
-  vertex.label.color = 'black', 
-  vertex.label.cex = 0.6, 
-  vertex.label.dist = 1.6,
-  edge.color = 'gray', 
-  # Set edge width proportional to the weight relative value.
-  edge.width = 3*E(network)$width ,
-  main = 'Bigram Count Network', 
-  sub = glue('Weight Threshold: {threshold}'), 
-  alpha = 50
-)
-
-threshold <- 1800
-
-network <-  bi.gram.count %>%
-  filter(weight > threshold) %>%
-  graph_from_data_frame(directed = FALSE)
-
-# Store the degree.
-V(network)$degree <- strength(graph = network)
-# Compute the weight shares.
-E(network)$width <- E(network)$weight/max(E(network)$weight)
-
-# Create networkD3 object.
-network.D3 <- igraph_to_networkD3(g = network)
-# Define node size.
-network.D3$nodes %<>% mutate(Degree = (1E-2)*V(network)$degree)
-# Define color group
-network.D3$nodes %<>% mutate(Group = 1)
-# Define edges width. 
-network.D3$links$Width <- 10*E(network)$width
-
-forceNetwork(
-  Links = network.D3$links, 
-  Nodes = network.D3$nodes, 
-  Source = 'source', 
-  Target = 'target',
-  NodeID = 'name',
-  Group = 'Group', 
-  opacity = 0.7,
-  Value = 'Width',
-  Nodesize = 'Degree', 
-  # We input a JavaScript function.
-  linkWidth = JS("function(d) { return Math.sqrt(d.value); }"), 
-  fontSize = 10,
-  opacityNoHover = 20
-)
-
+###começo da analise de sentimentos
 positive = scan("D:/documentos/rstudio/projetofinal/positive_words_pt.txt", what = 'character', comment.char = ';')
 negative = scan("D:/documentos/rstudio/projetofinal/negative_words_pt.txt", what = 'character', comment.char = ';')
 
@@ -251,12 +245,9 @@ score.sentiment = function(sentences, pos.words, neg.words, .progress='none')
   require(plyr)
   require(stringr)
   
-  # we are giving vector of sentences as input. 
-  # plyr will handle a list or a vector as an "l" for us
-  # we want a simple array of scores back, so we use "l" + "a" + "ply" = laply:
+  
   scores = laply(sentences, function(sentence, pos.words, neg.words) {
     
-    # clean up sentences with R's regex-driven global substitute, gsub() function:
     sentence = gsub('https://','',sentence)
     sentence = gsub('http://','',sentence)
     sentence = gsub('[^[:graph:]]', ' ',sentence)
@@ -264,53 +255,101 @@ score.sentiment = function(sentences, pos.words, neg.words, .progress='none')
     sentence = gsub('[[:cntrl:]]', '', sentence)
     sentence = gsub('\\d+', '', sentence)
     sentence = str_replace_all(sentence,"[^[:graph:]]", " ")
-    # and convert to lower case:
     sentence = tolower(sentence)
-    
-    # split into words. str_split is in the stringr package
     word.list = str_split(sentence, '\\s+')
-    # sometimes a list() is one level of hierarchy too much
     words = unlist(word.list)
-    
-    # compare our words to the dictionaries of positive & negative terms
     pos.matches = match(words, pos.words)
     neg.matches = match(words, neg.words)
-    
-    # match() returns the position of the matched term or NA
-    # we just want a TRUE/FALSE:
     pos.matches = !is.na(pos.matches)
     neg.matches = !is.na(neg.matches)
-    
-    # TRUE/FALSE will be treated as 1/0 by sum():
     score = sum(pos.matches) - sum(neg.matches)
-    
     return(score)
   }, pos.words, neg.words, .progress=.progress )
   
   scores.df = data.frame(score=scores, text=sentences)
   return(scores.df)
 }
-#####
+##### agora aplicando a função score para cada uma
 analysis <- score.sentiment(cleanText, positive, negative)
-# sentiment score frequency table
 table(analysis$score)
-###
+# antes
+analysisa <- score.sentiment(cleanTexta, positive, negative)
+table(analysisa$score)
+#depois
+analysisd <- score.sentiment(cleanTextd, positive, negative)
+table(analysisd$score)
+###grafico de analise de senteimentos
 analysis %>%
   ggplot(aes(x=score)) + 
-  geom_histogram(binwidth = 1, fill = "lightblue")+ 
-  ylab("Frequency") + 
-  xlab("sentiment score") +
-  ggtitle("Distribution of Sentiment scores of the tweets") +
+  geom_histogram(binwidth = 1, fill = "black")+ 
+  ylab("Frequência") + 
+  xlab("score de sentimentos") +
+  ggtitle("Distribuição de sentimentos por tweet") +
   ggeasy::easy_center_title()
-###
+##antes
+dev.off()
+analysisa %>%
+  ggplot(aes(x=score)) + 
+  geom_histogram(binwidth = 1, fill = "black")+ 
+  ylab("Frequência") + 
+  xlab("score de sentimentos") +
+  ggtitle("Distribuição de sentimentos por tweet, antes da invasão") +
+  ggeasy::easy_center_title()
+##depois
+analysisd %>%
+  ggplot(aes(x=score)) + 
+  geom_histogram(binwidth = 1, fill = "black")+ 
+  ylab("Frequência") + 
+  xlab("score de sentimentos") +
+  ggtitle("Distribuição de sentimentos por tweet, depois da invasão") +
+  ggeasy::easy_center_title()
+###sentimento barplot 
 
 neutral <- length(which(analysis$score == 0))
 positive <- length(which(analysis$score > 0))
 negative <- length(which(analysis$score < 0))
-Sentiment <- c("Positive","Neutral","Negative")
+Sentiment <- c("Positivo","Neutro","Negativo")
 Count <- c(positive,neutral,negative)
 output <- data.frame(Sentiment,Count)
 output$Sentiment<-factor(output$Sentiment,levels=Sentiment)
 ggplot(output, aes(x=Sentiment,y=Count))+
   geom_bar(stat = "identity", aes(fill = Sentiment))+
-  ggtitle("Barplot of Sentiment type of 4000 tweets")
+  ylab("Frequência") + 
+  xlab("Score de sentimentos") +
+  ggtitle("Barplot de sentimentos")
+
+#antes da invasão
+neutrala <- length(which(analysisa$score == 0))
+positivea <- length(which(analysisa$score > 0))
+negativea <- length(which(analysisa$score < 0))
+Sentiment <- c("Positivo","Neutro","Negativo")
+Count <- c(positivea,neutrala,negativea)
+outputa <- data.frame(Sentiment,Count)
+outputa$Sentiment<-factor(outputa$Sentiment,levels=Sentiment)
+ggplot(outputa, aes(x=Sentiment,y=Count))+
+  geom_bar(stat = "identity", aes(fill = Sentiment))+
+  ylab("Frequência") + 
+  xlab("Score de sentimentos") +
+  ggtitle("Barplot de sentimentos, antes da invasão")
+##depois da invasão
+neutrald <- length(which(analysisd$score == 0))
+positived <- length(which(analysisd$score > 0))
+negatived <- length(which(analysisd$score < 0))
+Sentiment <- c("Positivo","Neutro","Negativo")
+Count <- c(positived,neutrald,negatived)
+outputd <- data.frame(Sentiment,Count)
+outputd$Sentiment<-factor(outputd$Sentiment,levels=Sentiment)
+ggplot(outputd, aes(x=Sentiment,y=Count))+
+  geom_bar(stat = "identity", aes(fill = Sentiment))+
+  ylab("Frequência") + 
+  xlab("Score de sentimentos") +
+  ggtitle("Barplot de sentimentos, depois da invasão")
+#table
+write.table(output, file = "output.txt", sep = ",", quote = FALSE, row.names = F)
+write.table(outputa, file = "outputa.txt", sep = ",", quote = FALSE, row.names = F)
+write.table(outputd, file = "outputd.txt", sep = ",", quote = FALSE, row.names = F)
+#saving datasets
+
+
+
+
