125418fc70700f77220f5fa10cf2e5bc55796b7f
Samuel Efraim <samuefra@gmail.com>
2022-02-24 22:52:27 -0300

    projeto

--- projeto.R
+++ projeto.R
@@ -19,6 +19,16 @@ install.packages("magrittr")
 install.packages("tidyverse")
 install.packages("janeaustenr")
 install.packages("widyr")
+install.packages("httr")
+install.packages("lattice")
+install.packages("naivebayes")
+install.packages("lexiconPT")
+library(lexiconPT)
+library(naivebayes)
+library(lattice)
+library(Rstem)
+library(sentiment)
+library(httr)
 library(twitteR)
 library(ROAuth)
 library(hms)
@@ -42,20 +52,20 @@ library(magrittr)
 library(tidyverse)
 library(janeaustenr)
 library(widyr)
+#configurando API do twitter
 api_key <- "zbYQsOb0jFp3XiN3Wxzho4Cb4"
 api_secret <- "tEbDcHwdRsa4AVb5XSKcdc0Cmj659dNi0Q3MpEJwBnzhOUpjPO"
 access_token <- "1496765351215964161-OUdq8uwksz01Bm1rKPfT0ug0NuGlhX"
 access_token_secret <- "l7dBK3KMtOFrDyCzL7welnS8yZgpBmR0qSb8raukA3J3m"
 setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
-tweetsbr <- searchTwitter("vacina", n=4000, lang="pt")
-n.tweetbr <- length(tweetsbr)
-tweetsbr.df <- twListToDF(tweetsbr)
-tweetsbr.txt <- sapply(tweetsbr, function(t)t$getText())
-tweets <- searchTwitter("#vaccine", n=4000, lang="en")
-n.tweet <- length(tweets)
+
+#obtendo tweets em portugues
+tweets <- searchTwitter("Bolsonaro russia", n=20000, lang="pt")
+n.tweets <- length(tweets)
 tweets.df <- twListToDF(tweets)
 tweets.txt <- sapply(tweets, function(t)t$getText())
-tweets.txt <- str_replace_all(tweets.txt,"[^[:graph:]]", " ")
+
+# função limpando os tweets
 clean.text = function(x)
 {
   # convert to lower case
@@ -86,6 +96,8 @@ clean.text = function(x)
   x = str_replace_all(x,"[^[:graph:]]", " ")
   return(x)
 }
+
+#aplicando clean text e plotando por horas
 cleanText <- clean.text(tweets.txt)
 idx <- which(cleanText == " ")
 cleanText <- cleanText[cleanText != " "]
@@ -113,3 +125,215 @@ plt <- tweets.df %>%
 
 plt %>% ggplotly()
 
+##cloud
+text_corpus <- Corpus(VectorSource(cleanText))
+text_corpus <- tm_map(text_corpus, content_transformer(tolower))
+text_corpus <- tm_map(text_corpus, function(x)removeWords(x,stopwords("portuguese")))
+text_corpus <- tm_map(text_corpus, removeWords, c("rússia","bolsonaro"))
+tdm <- TermDocumentMatrix(text_corpus)
+tdm <- as.matrix(tdm)
+tdm <- sort(rowSums(tdm), decreasing = TRUE)
+tdm <- data.frame(word = names(tdm), freq = tdm)
+set.seed(123)
+wordcloud(text_corpus, min.freq = 1, max.words = 100, scale = c(3.1,1),
+          colors=brewer.pal(8, "Dark2"), random.color = T, random.order = F)
+###frquency
+ggplot(tdm[1:20,], aes(x=reorder(word, freq), y=freq)) + 
+  geom_bar(stat="identity") +
+  xlab("Terms") + 
+  ylab("Count") + 
+  coord_flip() +
+  theme(axis.text=element_text(size=7)) +
+  ggtitle('termos mais frequentes') +
+  ggeasy::easy_center_title()
+## bigram
+bi.gram.words <- tweets.df %>% 
+  unnest_tokens(
+    input = text, 
+    output = bigram, 
+    token = 'ngrams', 
+    n = 2
+  ) %>% 
+  filter(! is.na(bigram))
+
+bi.gram.words %>% 
+  select(bigram) %>% 
+  head(10)
+
+extra.stop.words <- c('https')
+stopwords.df <- tibble(
+  word = c(stopwords(kind = 'es'),
+           stopwords(kind = 'en'),
+           extra.stop.words)
+)
+
+bi.gram.words %<>% 
+  separate(col = bigram, into = c('word1', 'word2'), sep = ' ') %>% 
+  filter(! word1 %in% stopwords.df$word) %>% 
+  filter(! word2 %in% stopwords.df$word) %>% 
+  filter(! is.na(word1)) %>% 
+  filter(! is.na(word2)) 
+
+bi.gram.count <- bi.gram.words %>% 
+  dplyr::count(word1, word2, sort = TRUE) %>% 
+  dplyr::rename(weight = n)
+
+bi.gram.count %>% head()
+
+bi.gram.count %>% 
+  ggplot(mapping = aes(x = weight)) +
+  theme_light() +
+  geom_histogram() +
+  labs(title = "Bigram Weight Distribution")
+
+bi.gram.count %>% 
+  mutate(weight = log(weight + 1)) %>% 
+  ggplot(mapping = aes(x = weight)) +
+  theme_light() +
+  geom_histogram() +
+  labs(title = "Bigram log-Weight Distribution")
+
+threshold <- 1200
+
+# For visualization purposes we scale by a global factor. 
+ScaleWeight <- function(x, lambda) {
+  x / lambda
+}
+
+network <-  bi.gram.count %>%
+  filter(weight > threshold) %>%
+  mutate(weight = ScaleWeight(x = weight, lambda = 2E3)) %>% 
+  graph_from_data_frame(directed = FALSE)
+
+
+
+
+V(network)$degree <- strength(graph = network)
+
+# Compute the weight shares.
+E(network)$width <- E(network)$weight/max(E(network)$weight)
+
+plot(
+  network, 
+  vertex.color = 'lightblue',
+  # Scale node size by degree.
+  vertex.size = 2*V(network)$degree,
+  vertex.label.color = 'black', 
+  vertex.label.cex = 0.6, 
+  vertex.label.dist = 1.6,
+  edge.color = 'gray', 
+  # Set edge width proportional to the weight relative value.
+  edge.width = 3*E(network)$width ,
+  main = 'Bigram Count Network', 
+  sub = glue('Weight Threshold: {threshold}'), 
+  alpha = 50
+)
+
+threshold <- 1600
+
+network <-  bi.gram.count %>%
+  filter(weight > threshold) %>%
+  graph_from_data_frame(directed = FALSE)
+
+# Store the degree.
+V(network)$degree <- strength(graph = network)
+# Compute the weight shares.
+E(network)$width <- E(network)$weight/max(E(network)$weight)
+
+# Create networkD3 object.
+network.D3 <- igraph_to_networkD3(g = network)
+# Define node size.
+network.D3$nodes %<>% mutate(Degree = (1E-2)*V(network)$degree)
+# Define color group
+network.D3$nodes %<>% mutate(Group = 1)
+# Define edges width. 
+network.D3$links$Width <- 10*E(network)$width
+
+forceNetwork(
+  Links = network.D3$links, 
+  Nodes = network.D3$nodes, 
+  Source = 'source', 
+  Target = 'target',
+  NodeID = 'name',
+  Group = 'Group', 
+  opacity = 0.9,
+  Value = 'Width',
+  Nodesize = 'Degree', 
+  # We input a JavaScript function.
+  linkWidth = JS("function(d) { return Math.sqrt(d.value); }"), 
+  fontSize = 10,
+  zoom = TRUE, 
+  opacityNoHover = 1
+)
+
+positive = scan("D:/documentos/rstudio/projetofinal/positive_words_pt.txt", what = 'character', comment.char = ';')
+negative = scan("D:/documentos/rstudio/projetofinal/negative_words_pt.txt", what = 'character', comment.char = ';')
+
+score.sentiment = function(sentences, pos.words, neg.words, .progress='none')
+{
+  require(plyr)
+  require(stringr)
+  
+  # we are giving vector of sentences as input. 
+  # plyr will handle a list or a vector as an "l" for us
+  # we want a simple array of scores back, so we use "l" + "a" + "ply" = laply:
+  scores = laply(sentences, function(sentence, pos.words, neg.words) {
+    
+    # clean up sentences with R's regex-driven global substitute, gsub() function:
+    sentence = gsub('https://','',sentence)
+    sentence = gsub('http://','',sentence)
+    sentence = gsub('[^[:graph:]]', ' ',sentence)
+    sentence = gsub('[[:punct:]]', '', sentence)
+    sentence = gsub('[[:cntrl:]]', '', sentence)
+    sentence = gsub('\\d+', '', sentence)
+    sentence = str_replace_all(sentence,"[^[:graph:]]", " ")
+    # and convert to lower case:
+    sentence = tolower(sentence)
+    
+    # split into words. str_split is in the stringr package
+    word.list = str_split(sentence, '\\s+')
+    # sometimes a list() is one level of hierarchy too much
+    words = unlist(word.list)
+    
+    # compare our words to the dictionaries of positive & negative terms
+    pos.matches = match(words, pos.words)
+    neg.matches = match(words, neg.words)
+    
+    # match() returns the position of the matched term or NA
+    # we just want a TRUE/FALSE:
+    pos.matches = !is.na(pos.matches)
+    neg.matches = !is.na(neg.matches)
+    
+    # TRUE/FALSE will be treated as 1/0 by sum():
+    score = sum(pos.matches) - sum(neg.matches)
+    
+    return(score)
+  }, pos.words, neg.words, .progress=.progress )
+  
+  scores.df = data.frame(score=scores, text=sentences)
+  return(scores.df)
+}
+#####
+analysis <- score.sentiment(cleanText, positive, negative)
+# sentiment score frequency table
+table(analysis$score)
+###
+analysis %>%
+  ggplot(aes(x=score)) + 
+  geom_histogram(binwidth = 1, fill = "lightblue")+ 
+  ylab("Frequency") + 
+  xlab("sentiment score") +
+  ggtitle("Distribution of Sentiment scores of the tweets") +
+  ggeasy::easy_center_title()
+###
+
+neutral <- length(which(analysis$score == 0))
+positive <- length(which(analysis$score > 0))
+negative <- length(which(analysis$score < 0))
+Sentiment <- c("Positive","Neutral","Negative")
+Count <- c(positive,neutral,negative)
+output <- data.frame(Sentiment,Count)
+output$Sentiment<-factor(output$Sentiment,levels=Sentiment)
+ggplot(output, aes(x=Sentiment,y=Count))+
+  geom_bar(stat = "identity", aes(fill = Sentiment))+
+  ggtitle("Barplot of Sentiment type of 4000 tweets")
